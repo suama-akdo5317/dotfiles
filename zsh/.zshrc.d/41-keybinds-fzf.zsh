da78234ab0b6458b05153acfa24f8d302c8727b6
suama-akdo5317 <umaisuamaumai@icloud.com>
2026-03-16 23:14:59 +0900

    chore: update fzf keybinds

--- zsh/.zshrc.d/41-keybinds-fzf.zsh
+++ zsh/.zshrc.d/41-keybinds-fzf.zsh
@@ -7,6 +7,17 @@ if ! command -v fzf >/dev/null 2>&1; then
     return
 fi
 
+# fzf環境変数
+export FZF_DEFAULT_COMMAND="fd -H -E .git -E node_modules --color=always"
+export FZF_DEFAULT_OPTS="--ansi -e --ignore-case --layout=reverse --border=rounded --height 80%"
+export FZF_CTRL_T_COMMAND="fd --type f -H -E .git -E node_modules"
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=header,grid --line-range :100 {}'"
+export FZF_ALT_C_COMMAND="fd --type d -H -E .git -E node_modules"
+export FZF_ALT_C_OPTS="--preview 'eza {} -h -T -F --no-user --no-time --no-filesize --no-permissions --long | head -200'"
+
+# fzf公式キーバインド・補完の有効化（Ctrl+T, Alt+C が使えるようになる）
+source <(fzf --zsh)
+
 # =============================================================================
 # 履歴検索（Ctrl+R）
 # =============================================================================
